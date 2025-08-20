6272906a394c606216d5c45bcbe2cd93aad864e5
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-08-20 14:51:19 +0530

    Added the len calculation resolution

--- src/neetcode75/strings/LongestSubStringOfUniqueChars.java
+++ src/neetcode75/strings/LongestSubStringOfUniqueChars.java
@@ -1,19 +1,30 @@
 package neetcode75.strings;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class LongestSubStringOfUniqueChars {
     public static void main(String[] args) {
-        System.out.println("Longest String is:"+longestSubString("ThisIsTheStrongestPartnership"));
+        System.out.println("Longest String is:"+longestSubString("zxyzxyz"));
     }
-    public static String longestSubString(String str){
-        StringBuilder result=new StringBuilder();
+    public static int longestSubString(String s) {
+        if(s==null || s.length()==0) return 0;
 
-        HashSet<Character> characters = new HashSet<>();
-        for (Character c: str.toCharArray()){
-            if (!characters.add(c)) result.append(c);
+        int right=0,left=0,maxLen=0;
+        Set<Character> set =new HashSet<>();
+        int len=0;
+        while (right<s.length() && left<s.length()){
+            if(set.add(s.charAt(right))){
+                len = (right-left)+1;
+                right+=1;
+            }else{
+                set.remove(s.charAt(left));
+                left+=1;
+                len = (right-left)+1;
+            }
+            maxLen=maxLen>len?maxLen:len;
+            System.out.println("Right at:"+right+" Left:"+left+" MaxLen:"+maxLen);
         }
-
-        return String.valueOf(result);
+        return maxLen;
     }
 }
