8c15fde5040283f356702ba09fc8ac05edf625c2
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-08-30 09:55:04 +0530

    Added the camel Case logic correctly

--- src/strings/StringRebuilding.java
+++ src/strings/StringRebuilding.java
@@ -1,18 +1,29 @@
 package strings;
 
+import java.util.Locale;
+
 public class StringRebuilding {
     public static void main(String[] args) {
-        String strs = "ThiS NeeDs TO bE conVerTeD to SMalL LetTerS";
+        String strs = "ThiS NeeDs TO bE conVerTeD to cameL CaSE";
         strs = strs.toLowerCase();
         String[] values = strs.split(" ");
 
         StringBuilder stringBuilder = new StringBuilder();
         for (int i=0;i<values.length;i++){
-            if(i==values.length-1) stringBuilder.append(values[i]);
-            else {
-                stringBuilder.append(values[i]).append("_");
+            String temp=values[i];
+            if(i==values.length-1){
+                temp=temp.substring(0,1).toUpperCase()+temp.substring(1, temp.length());
+                stringBuilder.append(temp);
+            }
+            else if (i>0) {
+                temp=temp.substring(0,1).toUpperCase()+temp.substring(1, temp.length());
+                stringBuilder.append(temp).append("_");
+            } else {
+                stringBuilder.append(temp).append("_");
             }
 
         }
+
+        System.out.println("Changed Case to camel_case:"+stringBuilder);
     }
 }
