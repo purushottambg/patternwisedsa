9d98d072e244306360ee2163002dfe4c6103fa69
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-09-01 23:31:07 +0530

    Added the rev num program

---
+++ src/other/LargeNumerRev.java
@@ -0,0 +1,22 @@
+package other;
+
+
+public class LargeNumerRev {
+    public static void main(String[] args) throws Exception{
+        int num=32634934;
+        Integer finalValue;
+        String str = num+"";
+        char[] chars = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i=chars.length-1;i>=0;i--){
+            sb=sb.append(chars[i]);
+        }
+        try {
+            finalValue  = Integer.parseInt(sb.toString());
+        }catch (Exception e){
+            throw new Exception();
+        }
+        System.out.println("Reversed Value is:"+finalValue);
+    }
+
+}
