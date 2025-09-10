b7187c207d310bc8c31e0216306d9391e93d00b8
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2025-09-10 23:49:32 +0530

    Added the Array rearrangements program

---
+++ src/arrays/ReArrangeTheArray.java
@@ -0,0 +1,20 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ReArrangeTheArray {
+    public static void main(String[] args) {
+        int[] numArray = {2,3,4,5,6,7};
+        System.out.println("Before");
+        Arrays.stream(numArray).forEach(System.out::print);
+        for (int i = 0; i < numArray.length-1; i++) {
+            if(i%2==0 && numArray[i]<numArray[i+1]){
+                int temp = numArray[i];
+                numArray[i]=numArray[i+1];
+                numArray[i+1]=temp;
+            }
+        }
+        System.out.println("\nAfter");
+        Arrays.stream(numArray).forEach(System.out::print);
+    }
+}
